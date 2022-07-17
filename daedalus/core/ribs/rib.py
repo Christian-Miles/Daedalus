d617a4024f14d381ff464e5fa201902efcbfc0d2
Christian Miles <christianrmiles@gmail.com>
2022-07-17 22:29:22 +1000

    Refactored rotations to use a single method and matricies instead of seperated methods

--- daedalus/core/ribs/rib.py
+++ daedalus/core/ribs/rib.py
@@ -5,13 +5,12 @@ import matplotlib.pyplot as plt
 
 
 class Rib:
-    def __init__(self, aoa: float = -4.0, rot_x_axis: float = 0.0) -> None:
+    def __init__(self, aoa: float = 0, rot_x_axis: float = 0.0) -> None:
         self.rib_file = str(pathlib.Path(__file__).parents[2]) + "/defaults/ribs.dat"
         self.aoa = 0
-        self.arc_rot = 0
+        self.arc_ang = 0
         self.import_rib(self.rib_file)
-        self.apply_aoa(aoa)
-        self.test()
+        self.apply_rot(aoa=aoa, arc_ang=rot_x_axis)
 
     def import_rib(self, rib_file: str) -> None:
         with open(rib_file, "r") as f:
@@ -20,40 +19,35 @@ class Rib:
             point_list = []
 
             for line in lines:
-                values = np.float_(line.split())
+                values = line.split()
+                values.insert(1, 0.0)
+                values = np.float_(values)
                 values[0] = (values[0] - 1) * -1  # Flip and set tail to be 0 point
-                values = np.append(values, 0.0)
+                values = values[np.newaxis].T
                 point_list.append(values)
 
-            # Returns the individual points of a rib in the form of [x, y, z]
+            # Returns the individual points of a rib in the form of [[x], [y], [z]]
+            self.rib_points_original = np.array(point_list)
             self.rib_points = np.array(point_list)
 
-    def apply_aoa(self, aoa: float) -> None:
-        """Applies the angle of attack to the rib in 2D."""
-        change_list = [-self.aoa, aoa]
-        self.aoa = aoa
+    def apply_rot(self, aoa: float = None, arc_ang: float = None) -> None:
 
-        for apply in change_list:
-            for index, item in enumerate(self.rib_points):
-                aoa_rad = np.deg2rad(apply)
-                x_new = item[0] * np.cos(aoa_rad) - item[1] * np.sin(aoa_rad)
-                z_new = item[0] * np.sin(aoa_rad) + item[1] * np.cos(aoa_rad)
-                self.rib_points[index] = [x_new, z_new, item[2]]
+        self.aoa = aoa if aoa is not None else self.aoa
+        self.arc_ang = arc_ang if arc_ang is not None else self.arc_ang
 
-    def apply_arc(self, angle: float) -> None:
-        pass
+        aoa_rad = np.deg2rad(self.aoa)
+        arc_rad = np.deg2rad(self.arc_ang)
 
-    def apply_arc_rot(self, angle: float) -> None:
-        """Applies the arc rotation to the rib in 2D."""
-        change_list = [-self.arc_rot, angle]
-        self.arc_rot = angle
+        rotation_matrix = np.array(
+            [
+                [np.cos(aoa_rad), 0, -np.sin(aoa_rad)],
+                [0, np.cos(arc_rad), np.sin(arc_rad)],
+                [np.sin(aoa_rad), -np.sin(arc_rad), np.cos(aoa_rad) * np.cos(arc_rad)],
+            ]
+        )
 
-        for apply in change_list:
-            for index, item in enumerate(self.rib_points):
-                arc_rad = np.deg2rad(apply)
-                z_new = item[1] * np.cos(arc_rad) - item[2] * np.sin(arc_rad)
-                y_new = item[1] * np.sin(arc_rad) + item[2] * np.cos(arc_rad)
-                self.rib_points[index] = [item[0], y_new, z_new]
+        for index, item in enumerate(self.rib_points_original):
+            self.rib_points[index] = rotation_matrix @ item
 
     def visualize(self):
         """Purely for debugging, will be removed in later releases"""
@@ -61,10 +55,10 @@ class Rib:
         y = []
         z = []
 
-        for each in self.rib_points:
+        for each in self.rib_points.squeeze():
             x.append(each[0])
-            y.append(each[2])
-            z.append(each[1])
+            y.append(each[1])
+            z.append(each[2])
 
         fig = plt.figure(figsize=(12, 12))
         ax = fig.add_subplot(111, projection="3d")
